7635e98e8f0dc1f8ab4aabdfbd6e4d2d355d9b24
Mario da Costa Bacellar <mariobacellar@users.noreply.github.com>
2019-03-05 13:50:48 -0300

    Update servsim.js

--- servsim.js
+++ servsim.js
@@ -1,36 +1,40 @@
-// Um cache simples (já que é só um), para restorno imediato
+// A simple cache for immediate returns the files already loaded.
 const cache = [];
 
-// Modulo para gerenciamento de rotas. Se quiser troque pelo Express, Server.js, ... O importante são as funções na lib
+// Restify is as MVC framework for route management. If you want to you can switch to Express, Server.js, ... 
+// The important thing is the functions in lib.
 var restify = require('restify');
-
-// Modulo para subir o servidor
-var server      = restify.createServer({name:'Servsim :: Server Simulator v0.1 :: Mario Bacellar :: [https://www.linkedin.com/in/mariobacellar]'});
+var server = restify.createServer({name:'Servsim :: Service Simulator v0.1 :: Mario Bacellar :: [https://www.linkedin.com/in/mariobacellar]'});
     server.use(restify.plugins.bodyParser({ mapParams: false })); // Para poder pegar o JSON do body no post, put, ...
 
-// Modulo com as funções do Servsim
+// Servsim lib. 
 var servsim_lib = require('./servsim_modules/servsim_lib');
 
-// ************************************************
-// JSONs da pasta './client'
-// ************************************************
-// Função comum
-server.get  ('/client/select/:id' 			, function(req, res, next) { return servsim_lib.selectFileJsonById 			(req, res, next, cache	, 'client');});
-server.put	('/client'						, function(req, res, next) { return servsim_lib.saveFileJson				(req, res, next			, 'client');});
-// Função especifica para o campo 'phone_number'
-server.post ('/client/selectPhoneNumber'	, function(req, res, next) { return servsim_lib.selectFileJsonByPhoneNumber	(req, res, next, cache	, 'client');});
+
 
 // ************************************************
-// JSONs da pasta './product'
+// JSONs folder './client'
 // ************************************************
-// Função comum
+// Common functions: selectFileJsonById and saveFileJson
+server.get  ('/client/select/:id' 			, function(req, res, next) { return servsim_lib.selectFileJsonById 			(req, res, next, cache	, 'client');});
+server.put	('/client'						, function(req, res, next) { return servsim_lib.saveFileJson				(req, res, next			, 'client');});
+// A proper function for 'phone_number' field from JSON
+server.post ('/client/selectPhoneNumber'	, function(req, res, next) { return servsim_lib.selectFileJsonByPhoneNumber	(req, res, next, cache	, 'client');});
+
+
+
+
+// ************************************************
+// JSONs folder './product'
+// ************************************************
+// Common functions: selectFileJsonById and saveFileJson
 server.get  ('/product/select/:id'			, function(req, res, next) { return servsim_lib.selectFileJsonById	(req, res, next, cache	, 'product');});
 server.put	('/product'						, function(req, res, next) { return servsim_lib.saveFileJson		(req, res, next			, 'product');});
-// Função especifica para o campo 'code'
+// A proper function for 'code' field from JSON
 server.post ('/product/selectCode'			, function(req, res, next) { return servsim_lib.selectFileJsonByCode(req, res, next, cache	, 'product');});
 
 
 
 
-// Sobre o servidor na 8090
+// Running on 8090
 server.listen(8089, function(){ console.log('%s listening at %s', server.name, server.url);});
